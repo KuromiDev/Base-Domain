7657d06deb26c0c536e2073895e4901b3bb00d3c
KuromiDev <AliceDevelopments@proton.me>
2025-06-26 15:31:03 +0100

    Redirect for one project

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 function Redirect() {
     window.open("https://mail.google.com/mail/u/0/?view=cm&fs=1&to=enquiry@alicedevelopments.com&su=Enquiry Regarding Alice Developments", "_blank");
     }
+
+document.querySelector('#ProjectOne').addEventListener('click', function(e) {
+    e.preventDefault();
+    setTimeout(function() {
+        window.location.href = 'projectOne.html';
+    }, 250); 
+}, false);
